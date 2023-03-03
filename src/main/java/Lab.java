1b48b3f7106c92e5a3c2d5aac79ffdbcc7f1974c
Josh B <jborger21@gmail.com>
2023-03-03 00:05:42 +0000

    JB If Statement 3/2/2023

--- src/main/java/Lab.java
+++ src/main/java/Lab.java
@@ -1,3 +1,4 @@
+import java.math.RoundingMode;
 
 public class Lab {
     /**
@@ -20,6 +21,11 @@ public class Lab {
      * @return x if bool is true. if bool is false, return y.
      */
     public int ifExample(boolean bool, int x, int y){
+        if (bool == true){
+            return x;
+        }else if (bool == false){
+            return y;
+        }
         return 0;
     }
 }
